ac56e8a523748d8587b1ebf68b438b66f538ffda
NyNguyen <tramny2002@yahoo.com>
2020-01-30 16:05:21 -0500

    lab 3 version 1

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,56 +1,64 @@
 package com.example.androidlabs;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.Switch;
-import android.widget.Toast;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import static android.app.ProgressDialog.show;
-
 public class MainActivity extends AppCompatActivity {
-    private CheckBox checkbox;
-    private Switch aSwitch;
+    //  private EditText pwInput;
+    private EditText emailInput;
     private Button button;
 
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_relative);
-     //setContentView(R.layout.activity_main_linear);
-       // setContentView(R.layout.activity_main_grid);
-        checkbox = findViewById(R.id.checkBox2);
-        aSwitch = findViewById(R.id.switch2);
-        button = findViewById(R.id.button3);
-        button.setOnClickListener(v-> Toast.makeText( MainActivity.this, "Here is more information", Toast.LENGTH_LONG).show());
-        checkbox.setOnCheckedChangeListener((buttonView, isChecked)-> {
-            if (checkbox.isChecked()){
-                Snackbar.make(checkbox, getResources().getString(R.string.check_message_true), Snackbar.LENGTH_LONG)
-                        .setAction(getResources().getString(R.string.setAction_message),
-                                click -> buttonView.setChecked(!isChecked) ).show();
-            }else{
-                Snackbar.make(checkbox, getResources().getString(R.string.check_message_false), Snackbar.LENGTH_LONG)
-                        .setAction(getResources().getString(R.string.setAction_message),
-                                click -> buttonView.setChecked(!isChecked) ).show();
-            }
-        });
-        aSwitch.setOnCheckedChangeListener((buttonView, isChecked)-> {
-            if (aSwitch.isChecked()){
-                Snackbar.make(aSwitch, getResources().getString(R.string.switch_message_true), Snackbar.LENGTH_LONG)
-                        .setAction(getResources().getString(R.string.setAction_message),
-                                click -> buttonView.setChecked(!isChecked) ).show();
-            }else{
-                Snackbar.make(aSwitch, getResources().getString(R.string.switch_message_false), Snackbar.LENGTH_LONG)
-                        .setAction(getResources().getString(R.string.setAction_message),
-                                click -> buttonView.setChecked(!isChecked) ).show();
-            }
-        });
+        setContentView(R.layout.activity_main_lab3);
+        emailInput = findViewById(R.id.email1);
+        //    pwInput = findViewById(R.id.passWord);
 
+
+
+        SharedPreferences ref = getSharedPreferences ( "activity_main_lab3", Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = ref.edit();
+        emailInput.setText(ref.getString("email", ""));
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(v-> {
+            onPause();
+            Intent goToProfile = new Intent(MainActivity.this, ProfileActivity.class);
+            goToProfile.putExtra( "EMAIL",emailInput.getText().toString());
+            startActivity(goToProfile);
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences ref = getSharedPreferences( "activity_main_lab3",Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = ref.edit();
+        edit.putString("email", emailInput.getText().toString()).commit();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
